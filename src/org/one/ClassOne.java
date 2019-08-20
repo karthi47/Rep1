216383e68ad6376481069634ca1ed6b9bccc7494
karthi47 <karthickdurai.it@gmail.com>
2019-08-20 11:13:00 +0530

    new code dayone

---
+++ src/org/one/ClassOne.java
@@ -0,0 +1,7 @@
+package org.one;
+
+public class ClassOne {
+public static void main(String[] args) {
+	System.out.println("sample 1");
+}
+}
---
+++ src/org/one/ClassTwo.java
@@ -0,0 +1,6 @@
+package org.one;
+
+public class ClassTwo {
+	public static void main(String[] args) {
+		System.out.println("sample 2");
+}}
